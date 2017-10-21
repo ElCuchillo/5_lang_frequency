af078c9dd9bbbd02b89622adde452cc0dc78deba
ElCuchillo <32874706+ElCuchillo@users.noreply.github.com>
2017-10-21 17:39:23 +0300

    магические числа ликвидированы

--- lang_frequency.py
+++ lang_frequency.py
@@ -8,15 +8,15 @@ def load_data(file_path):
         with open(file_path, 'r') as f:
             word_pattern = re.compile('\w+', re.IGNORECASE)
             parsed_text = word_pattern.findall(f.read())
-    except FileNotFoundError:
-        return None, 1
+    except FileNotFoundError as load_error:
+        return None, load_error
     else:
         return parsed_text, None
 
 
 def get_most_frequent_words(parsed_text):
     top_10_words = collections.Counter(parsed_text).most_common(10)
-    # print(top_10_words)
+    #print(top_10_words)
     words_list = []
     for word, frequency in top_10_words:
         words_list.append(word)
@@ -27,7 +27,7 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         parsed_text, load_error = load_data(sys.argv[1])
         if load_error:
-            print('No such file or directory: ', sys.argv[1])
+            print(load_error)
         else:
             print("The 10 most frequently used words in the {} are: {}".format(sys.argv[1], \
                                                                                get_most_frequent_words(parsed_text)))
