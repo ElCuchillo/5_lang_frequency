96e1355191c3d5a11951f0bee0d6ebbeb4f444d9
ElCuchillo <32874706+ElCuchillo@users.noreply.github.com>
2017-10-23 00:02:14 +0300

    Update lang_frequency.py

--- lang_frequency.py
+++ lang_frequency.py
@@ -2,26 +2,19 @@ import sys
 import re
 import collections
 
-# The constant indicates quantity of the most common words to be outputed
-HOW_MANY_WORDS = 10
-
 
 def load_data(file_path):
     try:
-        with open(file_path, 'r') as data:
-            word_pattern = re.compile('\w+', re.IGNORECASE)
-            parsed_text = word_pattern.findall(data.read())
+        with open(file_path, 'r') as parced_file:
+            return parced_file.read()
 
     except FileNotFoundError:
         return None
 
-    else:
-        return parsed_text
 
-
-def get_most_frequent_words(parsed_text, words_qty):
-
-    return collections.Counter(parsed_text).most_common(words_qty)
+def get_most_frequent_words(parsed_file, how_many_words=10):
+    words_list = re.findall(r'\w+', parsed_file, re.IGNORECASE + re.UNICODE)
+    return collections.Counter(words_list).most_common(how_many_words)
 
 
 if __name__ == '__main__':
@@ -31,11 +24,11 @@ if __name__ == '__main__':
         if not parsed_text:
             print("File or direcory {} not found".format(sys.argv[1]))
         else:
-            top_10_words = get_most_frequent_words(parsed_text, HOW_MANY_WORDS)
-            print("{} most сommon words in the {} are (word, qty):"
-                  .format(HOW_MANY_WORDS, sys.argv[1]))
-            for word, qty in top_10_words:
-                print('{:.<20}{:<3}'.format(word, qty))
+            top_10_words = get_most_frequent_words(parsed_text)
+            print("Most сommon words in the {} are (word, frequency):"
+                  .format(sys.argv[1]))
+            for word, frequency in top_10_words:
+                print('{:.<20}{:<3}'.format(word, frequency))
 
     else:
         print("Using: python3 lang_frequency.py <path_to_file>")
