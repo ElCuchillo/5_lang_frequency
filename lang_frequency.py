5707ff3c6d7e597c1bea58082912aeca50546b4e
ElCuchillo <32874706+ElCuchillo@users.noreply.github.com>
2017-10-22 12:21:52 +0300

    исправлен вывод, переименована переменная, PEP8

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,35 +1,41 @@
-import re
 import sys
+import re
 import collections
+
+# The constant indicates quantity of the most common words to be outputed
+HOW_MANY_WORDS = 10
 
 
 def load_data(file_path):
     try:
-        with open(file_path, 'r') as f:
+        with open(file_path, 'r') as data:
             word_pattern = re.compile('\w+', re.IGNORECASE)
-            parsed_text = word_pattern.findall(f.read())
-    except FileNotFoundError as load_error:
-        return None, load_error
+            parsed_text = word_pattern.findall(data.read())
+
+    except FileNotFoundError:
+        return None
+
     else:
-        return parsed_text, None
+        return parsed_text
 
 
-def get_most_frequent_words(parsed_text,how_many_words=10):
-    top_10_words = collections.Counter(parsed_text).most_common(how_many_words)
-    #print(top_10_words)
-    words_list = []
-    for word, frequency in top_10_words:
-        words_list.append(word)
-    return words_list
+def get_most_frequent_words(parsed_text, words_qty):
+
+    return collections.Counter(parsed_text).most_common(words_qty)
 
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
-        parsed_text, load_error = load_data(sys.argv[1])
-        if load_error:
-            print(load_error)
+        parsed_text = load_data(sys.argv[1])
+
+        if not parsed_text:
+            print("File or direcory {} not found".format(sys.argv[1]))
         else:
-            print("The 10 most frequently used words in the {} are: {}".format(sys.argv[1], \
-                                                                               get_most_frequent_words(parsed_text)))
+            top_10_words = get_most_frequent_words(parsed_text, HOW_MANY_WORDS)
+            print("{} most сommon words in the {} are (word, qty):"
+                  .format(HOW_MANY_WORDS, sys.argv[1]))
+            for word, qty in top_10_words:
+                print('{:.<20}{:<3}'.format(word, qty))
+
     else:
-        print("Using: python3 lang_frequency.py <path to file>")
+        print("Using: python3 lang_frequency.py <path_to_file>")
