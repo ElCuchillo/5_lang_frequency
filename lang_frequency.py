601706a61855d7ca2d34e346e9f30ff0661b9ab5
ElCuchillo <32874706+ElCuchillo@users.noreply.github.com>
2017-10-23 12:23:57 +0300

    copy-past починился

--- lang_frequency.py
+++ lang_frequency.py
@@ -5,7 +5,7 @@ import collections
 
 def load_data(file_path):
     try:
-        with open(file_path, 'r') as loded_file:
+        with open(file_path, 'r') as loaded_file:
             return loaded_file.read()
 
     except FileNotFoundError:
@@ -13,18 +13,18 @@ def load_data(file_path):
 
 
 def get_most_frequent_words(parsed_file, how_many_words=10):
-    words_list = re.findall(r'\w+', parsed_file, re.IGNORECASE + re.UNICODE)
+    words_list = re.findall(r'\w+', parsed_file)
     return collections.Counter(words_list).most_common(how_many_words)
 
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
-        parsed_text = load_data(sys.argv[1])
+        loaded_file = load_data(sys.argv[1])
 
-        if not parsed_text:
+        if not loaded_file:
             print("File or direcory {} not found".format(sys.argv[1]))
         else:
-            most_common_words = get_most_frequent_words(parsed_text)
+            most_common_words = get_most_frequent_words(loaded_file)
             print("Most сommon words in the {} are (word, frequency):"
                   .format(sys.argv[1]))
             for word, frequency in most_common_words:
